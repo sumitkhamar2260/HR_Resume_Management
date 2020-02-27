5a929c5c532732585dc670b5c4618197d6d10ccd
Shubham Shah <shahshubham60@gmail.com>
2020-02-27 19:27:16 +0530

    completed

--- app/src/main/java/com/example/hrresumemanagement/ShowResume.java
+++ app/src/main/java/com/example/hrresumemanagement/ShowResume.java
@@ -1,5 +1,6 @@
 package com.example.hrresumemanagement;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -19,14 +20,20 @@ import java.util.Map;
 
 public class ShowResume extends AppCompatActivity {
     Intent intent;
-    String name,uid,mobile,email,city,state,degree,fieldOfStudy;
-    ArrayList<String> c_name,c_location,j_title,s_date,e_date;
+    String name,uid,mobile,email,city,state;
+    ArrayList<String> c_name,c_location,j_title,s_date,e_date,degree,fieldOfStudy,skills;
     DatabaseReference databaseReference;
-    TextView name_t,email_t,mobile_t,city_state,skill,education;
+    TextView name_t,email_t,mobile_t,city_state,skill,education,experience;
+    ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_resume);
+       /* ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setVisibility(View.VISIBLE);
+       */
+        progressDialog = new ProgressDialog(ShowResume.this);
+        progressDialog.setMessage("Loading");
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         intent = getIntent();
@@ -42,9 +49,19 @@ public class ShowResume extends AppCompatActivity {
         mobile_t = findViewById(R.id.mobile);
         city_state = findViewById(R.id.city_state);
         skill = findViewById(R.id.skill);
+        skills = new ArrayList<>();
         education = findViewById(R.id.education);
+        experience = findViewById(R.id.experience);
+        degree = new ArrayList<>();
+        fieldOfStudy = new ArrayList<>();
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        progressDialog.show();
         retrievePersonalDetails();
+        retrieveEducationalDetails();
+        retrieveExperienceDetails();
+        retrieveSkillDetails();
+        progressDialog.dismiss();
+       // progressBar.setVisibility(View.GONE);
     }
     public void retrievePersonalDetails(){
         databaseReference.child("users").child(uid).child("Personal details").addValueEventListener(new ValueEventListener() {
@@ -57,8 +74,8 @@ public class ShowResume extends AppCompatActivity {
                 state = map.get("State");
                 name_t.setText(name);
                 email_t.setText(email);
+                mobile_t.setText(mobile);
                 city_state.setText(city+","+state);
-
             }
 
             @Override
@@ -71,14 +88,18 @@ public class ShowResume extends AppCompatActivity {
         databaseReference.child("users").child(uid).child("Education").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                /*for(DataSnapshot details : dataSnapshot.getChildren()){
-                    for(DataSnapshot details1 : da)
-                    Map<String,String> map = (Map)details.getValue();
-                    degree = map.get("Degree");
-                    fieldOfStudy = map.get("Field Of Study");
-                }*/
-            }
+                for(DataSnapshot details : dataSnapshot.getChildren()) {
+                    //for (DataSnapshot details1 : details.getChildren()) {
+                        Map<String, String> map = (Map) details.getValue();
+                        degree.add( map.get("Degree"));
+                        fieldOfStudy.add( map.get("Field Of Study"));
 
+                    //}
+                }
+                for (int i = 0;i<degree.size();i++){
+                    education.append(degree.get(i)+" in " +fieldOfStudy.get(i)+"\n");
+                }
+            }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
@@ -86,7 +107,7 @@ public class ShowResume extends AppCompatActivity {
         });
     }
     public void retrieveExperienceDetails(){
-        databaseReference.child("users").child(uid).child("Experience").addValueEventListener(new ValueEventListener() {
+        databaseReference.child("users").child(uid).child("Experience").child(uid).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for(DataSnapshot details : dataSnapshot.getChildren()){
@@ -96,6 +117,26 @@ public class ShowResume extends AppCompatActivity {
                     j_title.add(map.get("Job Title"));
                     s_date.add(map.get("Start Date"));
                     e_date.add(map.get("End Date"));
+                    System.out.println(c_name);
+                }
+                for(int i =0;i<c_name.size();i++){
+                    experience.append(j_title.get(i)+"\n"+c_name.get(i)+"\n"+s_date.get(i)+" - "+e_date.get(i)+"\n");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public void retrieveSkillDetails(){
+        databaseReference.child("users").child(uid).child("Skills").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Map<String,String> map = (Map)dataSnapshot.getValue();
+                for(int i = 1;i<dataSnapshot.getChildrenCount();i++){
+                    skill.append(map.get("Skill "+i)+"\n");
                 }
             }
 
