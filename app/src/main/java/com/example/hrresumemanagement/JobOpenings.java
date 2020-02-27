631ccdeca9c96b4e57529baa1fcae699617947a0
Shubham Shah <shahshubham60@gmail.com>
2020-02-27 12:33:08 +0530

    completed

--- app/src/main/java/com/example/hrresumemanagement/JobOpenings.java
+++ app/src/main/java/com/example/hrresumemanagement/JobOpenings.java
@@ -11,7 +11,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class JobOpenings extends AppCompatActivity {
-    ArrayList<String> jobtitle,location,applications;
+    ArrayList<String> jobtitle,location,applications,jobID;
     RecyclerView rv;
     FirebaseAuth firebaseAuth;
     FirebaseDatabase fd;
@@ -35,8 +34,8 @@ public class JobOpenings extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton new_recruitment = findViewById(R.id.new_recruitment);
+        new_recruitment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -48,6 +47,7 @@ public class JobOpenings extends AppCompatActivity {
         jobtitle=new ArrayList<>();
         location=new ArrayList<>();
         applications=new ArrayList<>();
+        jobID = new ArrayList<>();
         firebaseAuth=FirebaseAuth.getInstance();
         fd=FirebaseDatabase.getInstance();
         ref=fd.getReference("Companies").child(firebaseAuth.getCurrentUser().getUid()).child("Job Openings");
@@ -62,11 +62,12 @@ public class JobOpenings extends AppCompatActivity {
                     jobtitle.add( String.valueOf(ob.get("JobTitle")));
                     System.out.println("Job title is "+ob.get("JobTitle"));
                     location.add( String.valueOf(ob.get("Location")));
-                    applications.add("1");
+                    applications.add(String.valueOf(jobdetails.child("Applicants").getChildrenCount()));
+                    jobID.add(String.valueOf(ob.get("JobId")));
                 }
                 RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getApplicationContext());
                 rv.setLayoutManager(layoutManager);
-                jobdetailrow ob=new jobdetailrow(getApplicationContext(),jobtitle,location,applications);
+                jobdetailrow ob=new jobdetailrow(getApplicationContext(),jobtitle,location,applications,jobID);
                 rv.setAdapter(ob);
             }
 
@@ -78,7 +79,7 @@ public class JobOpenings extends AppCompatActivity {
         });
         RecyclerView.LayoutManager layoutManager=new LinearLayoutManager(getApplicationContext());
         rv.setLayoutManager(layoutManager);
-        jobdetailrow ob=new jobdetailrow(getApplicationContext(),jobtitle,location,applications);
+        jobdetailrow ob=new jobdetailrow(getApplicationContext(),jobtitle,location,applications,jobID);
         rv.setAdapter(ob);
     }
 
--- app/src/main/java/com/example/hrresumemanagement/jobdetailrow.java
+++ app/src/main/java/com/example/hrresumemanagement/jobdetailrow.java
@@ -1,23 +1,23 @@
 package com.example.hrresumemanagement;
 
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class jobdetailrow extends RecyclerView.Adapter<jobdetailrow.MyViewHolder>  {
     ArrayList<String> jobtitle;
     ArrayList<String> location;
     ArrayList<String> applications;
+    ArrayList<String> jobID;
     Context context;
     public class MyViewHolder extends RecyclerView.ViewHolder{
         TextView jobtit,loc,apps;
@@ -30,11 +30,12 @@ public class jobdetailrow extends RecyclerView.Adapter<jobdetailrow.MyViewHolder
             video_card = (CardView)itemView.findViewById(R.id.video_card);
         }
     }
-    public jobdetailrow(Context context,ArrayList<String> jobtitle, ArrayList<String> location, ArrayList<String> applications){
+    public jobdetailrow(Context context,ArrayList<String> jobtitle, ArrayList<String> location, ArrayList<String> applications,ArrayList<String> jobID){
         this.jobtitle = jobtitle;
         this.location = location;
         this.applications = applications;
         this.context = context;
+        this.jobID = jobID;
     }
     public jobdetailrow.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.job_row,parent,false);
@@ -47,6 +48,15 @@ public class jobdetailrow extends RecyclerView.Adapter<jobdetailrow.MyViewHolder
         holder.jobtit.setText(jobtit);
         holder.loc.setText(loc);
         holder.apps.setText(apps);
+        holder.video_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,RecievedApplications.class);
+                intent.putExtra("Job ID",jobID.get(position));
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });
     }
     public int getItemCount() {
         return jobtitle.size();
